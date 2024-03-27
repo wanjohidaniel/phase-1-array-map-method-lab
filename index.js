3d2d79a4ab40c9234755aa86f3fa93c1cba94623
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-27 05:01:06 -0700

    Array Map Method

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    let words = tutorial.split(' ');
+    let capitalizedWords = words.map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
 }
